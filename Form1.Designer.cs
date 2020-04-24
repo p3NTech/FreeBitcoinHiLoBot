3a22c5584b08046ca983dbd6ce97c0858eece510
Mikhail Solovyov <62037760+miksolovoff@users.noreply.github.com>
2020-04-24 18:27:35 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace FreeBitcoin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.instructions = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // instructions
+            // 
+            this.instructions.Location = new System.Drawing.Point(12, 74);
+            this.instructions.Name = "instructions";
+            this.instructions.Size = new System.Drawing.Size(126, 46);
+            this.instructions.TabIndex = 0;
+            this.instructions.Text = "Выбрать файл с инструкцией";
+            this.instructions.UseVisualStyleBackColor = true;
+            this.instructions.Click += new System.EventHandler(this.instructions_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Зайти на freebitco.in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Играть!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 357);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1172, 235);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(126, 46);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Выбрать файл для записи логов";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(918, 207);
+            this.label1.TabIndex = 7;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 262);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 46);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Выйти из программы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(404, 314);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(301, 26);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Отчет по каждому из типов игр";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(827, 244);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(324, 96);
+            this.richTextBox2.TabIndex = 10;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 595);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.instructions);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "FreeBitcoinBot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button instructions;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.IO;
+using OpenQA.Selenium;
+using System.Globalization;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+public struct ParamsForBot
+{
+    public string TypeOfGame;
+    public int HowManyRates;
+    public string SumOfRate;
+    public string PercentOfRate;
+}
+public struct GameStruct
+{
+    public string[] TimeNow;
+    public bool[] Win;
+    public string[] Balance;
+}
+namespace FreeBitcoin
+{
+    public partial class Form1 : Form
+    {
+        ParamsForBot[] Params = new ParamsForBot[1000];
+        GameStruct[] Game = new GameStruct[1000];
+        IWebDriver Browser;
+        StreamWriter Writer;
+        FileStream FileTxt1;
+        public string GoodDecSeparator, BadDecSeparator;
+
+        public Form1()
+        {
+            InitializeComponent();
+            NumberFormatInfo nfi = CultureInfo.CurrentCulture.NumberFormat;
+            GoodDecSeparator = nfi.NumberDecimalSeparator;
+            BadDecSeparator = (GoodDecSeparator == ".") ? "," : ".";
+        }    
+    public void instructions_Click(object sender, EventArgs e)
+        {            
+            int j = 1;
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "Файлы .txt|*.txt";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(OPF.FileName);
+                string InstructionsPath = OPF.FileName;
+                FileStream FileTxt = new FileStream(InstructionsPath, FileMode.Open);
+                StreamReader Reader = new StreamReader(FileTxt);
+                string temp;
+                while ((temp = Reader.ReadLine()) != null) // считывание из файла и заполнение массива структур
+                {
+                    string[] ParamsArr = new string[4];
+                    try
+                    {
+                        ParamsArr = temp.Split(' '); // массив из строк в каждой строке файла
+
+                        switch (ParamsArr[0][0]) // Вверх или вниз
+                        {
+                            case '!':
+                                Params[j].TypeOfGame = Params[j - 1].TypeOfGame;
+                                break;
+                            default:
+                                Params[j].TypeOfGame = ParamsArr[0];
+                                break;
+                        }
+                        switch (ParamsArr[1][0]) // Количество ставок
+                        {
+                            case '!':
+                                Params[j].HowManyRates = Params[j - 1].HowManyRates;
+                                break;
+                            default:
+                                Params[j].HowManyRates = Int32.Parse(ParamsArr[1]);
+                                break;
+                        }
+                        switch (ParamsArr[2][0]) // Cтрока с суммой ставки
+                        {
+                            case '!':
+                                Params[j].SumOfRate = Params[j - 1].SumOfRate;
+                                break;
+                            default:
+                                if (ParamsArr[2][ParamsArr[2].Length - 1] != '!') Params[j].SumOfRate = ParamsArr[2];
+                                else
+                                {
+                                    string t = ParamsArr[2].Substring(0, ParamsArr[2].Length - 1);
+                                    Params[j].SumOfRate = t;
+                                }
+                                break;
+                        }
+                        switch (ParamsArr[3][0]) // Строка с процентом выигрыша
+                        {
+                            case '!':
+                                Params[j].PercentOfRate = Params[j - 1].PercentOfRate;
+                                break;
+                            default:
+                                if (ParamsArr[3][ParamsArr[3].Length - 1] != '!') Params[j].PercentOfRate = ParamsArr[3];
+                                else
+                                {
+                                    string t = ParamsArr[3].Substring(0, ParamsArr[3].Length - 1);
+                                    Params[j].PercentOfRate = t;
+                                }
+                                break;
+                        }
+                        j++;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Проверьте правильность тектового документа", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    }
+                }
+                Params[j + 1].TypeOfGame = null; // остановка
+            }
+            else MessageBox.Show("Ошибка открытия файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);   
+        }
+        BackgroundWorker GoToSite;
+        BackgroundWorker Work;
+        private async void GoToSiteClick()
+        {
+            Browser = new OpenQA.Selenium.Chrome.ChromeDriver();
+            Browser.Manage().Window.Maximize();
+            Browser.Navigate().GoToUrl("https://freebitco.in/");
+            Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+            IWebElement LoginTab = Browser.FindElement(By.XPath("//li[@class='login_menu_button']"));
+            LoginTab.Click();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GoToSite = new BackgroundWorker();
+                GoToSite.DoWork += (obj, ea) => GoToSiteClick();
+                GoToSite.RunWorkerAsync(); 
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка на сайте", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }  
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                async void work()
+                {
+                    Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+                    IWebElement High = Browser.FindElement(By.Id("double_your_btc_bet_hi_button")); // Hi
+                    IWebElement Low = Browser.FindElement(By.Id("double_your_btc_bet_lo_button")); // Lo
+                    IWebElement Bal = Browser.FindElement(By.Id("balance")); // Balance
+                    IWebElement d1 = Browser.FindElement(By.Id("multiplier_first_digit")); // 1 цифра
+                    IWebElement d2 = Browser.FindElement(By.Id("multiplier_second_digit")); // 2 цифра
+                    IWebElement d3 = Browser.FindElement(By.Id("multiplier_third_digit")); // 3 цифра
+                    IWebElement d4 = Browser.FindElement(By.Id("multiplier_fourth_digit")); // 4 цифра
+                    IWebElement d5 = Browser.FindElement(By.Id("multiplier_fifth_digit")); // 5 цифра
+                    IWebElement More = Browser.FindElement(By.XPath("//span[@class='gt bold gt_lt_span']"));
+                    IWebElement Less = Browser.FindElement(By.XPath("//span[@class='lt bold gt_lt_span']"));
+                    int WinsAll = 0, LosesAll = 0;
+                    for (int i = 1; i < 1000; i++)
+                    {
+                        if (Params[i].TypeOfGame == null) break;
+                        Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+                        IWebElement SumOfRate = Browser.FindElement(By.Id("double_your_btc_stake")); // поле с суммой ставки
+                        SumOfRate.SendKeys("\b\b\b\b\b\b\b\b\b\b");
+                        SumOfRate.SendKeys(Params[i].SumOfRate);
+                        IWebElement PercentOfRate = Browser.FindElement(By.Id("double_your_btc_win_chance")); // поле с процентом выигрыша
+                        PercentOfRate.SendKeys("\b\b\b\b\b\b");
+                        PercentOfRate.SendKeys(Params[i].PercentOfRate);
+                        Game[i].Balance = new string[Params[i].HowManyRates];
+                        Game[i].Win = new bool[Params[i].HowManyRates];
+                        Game[i].TimeNow = new string[Params[i].HowManyRates];
+                        string History = "";
+                        switch (Params[i].TypeOfGame)
+                        {
+                            case "u":
+                                for (int j = 0; j < Params[i].HowManyRates; j++)
+                                {
+                                    //System.Threading.Thread.Sleep(500);
+                                    Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+                                    Game[i].Balance[j] = Bal.Text;
+                                    //System.Threading.Thread.Sleep(250);
+                                    string number = ""; // Результат игры
+                                    if (d1.Text != "0") number += d1.Text;
+                                    number += d2.Text;
+                                    number += d3.Text;
+                                    number += d4.Text;
+                                    number += d5.Text;
+                                    Game[i].Win[j] = (Convert.ToInt32(number) > Convert.ToInt32(More.Text)) ? true : false;
+                                    Game[i].TimeNow[j] = DateTime.Now.ToLongTimeString();
+                                    string TempWin = Game[i].Win[j] ? "Выигрыш" : "Проигрыш";
+                                    string end = Game[i].Win[j] ? "1" : "0";
+                                    History += end;
+                                    Writer.WriteLine(i.ToString() + ". " + Game[i].TimeNow[j] + " " + TempWin + "   Баланс:" + Game[i].Balance[j] + "\n");
+                                    High.Click();
+                                }
+                                break;
+                            case "d":
+                                for (int j = 0; j < Params[i].HowManyRates; j++)
+                                {
+                                    //System.Threading.Thread.Sleep(500);
+                                    Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+                                    Game[i].Balance[j] = Bal.Text;
+                                    //System.Threading.Thread.Sleep(250);
+                                    string number = ""; // Результат игры
+                                    if (d1.Text != "0") number += d1.Text;
+                                    number += d2.Text;
+                                    number += d3.Text;
+                                    number += d4.Text;
+                                    number += d5.Text;
+                                    Game[i].Win[j] = (Convert.ToInt32(number) < Convert.ToInt32(Less.Text)) ? true : false;
+                                    Game[i].TimeNow[j] = DateTime.Now.ToLongTimeString();
+                                    string TempWin = Game[i].Win[j] ? "Выигрыш" : "Проигрыш";
+                                    string end = Game[i].Win[j] ? "1" : "0";
+                                    History += end;
+                                    Writer.WriteLine(i.ToString() + ". " + Game[i].TimeNow[j] + " " + TempWin + "   Баланс:" + Game[i].Balance[j] + "\n");
+                                    Low.Click();
+                                }
+                                break;
+                        }
+                        int WinMax = 0, LoseMax = 0, temp1 = 0, temp2 = 0, Wins = 0, Loses = 0;
+                        for (int k = 0; k < History.Length; k++)
+                        {
+                            if (History[k] == '1')
+                            {
+                                Wins++;
+                                if (temp2 > LoseMax) LoseMax = temp2;
+                                temp2 = 0;
+                                temp1++;
+                            }
+                            else
+                            {
+                                Loses++;
+                                if (temp1 > WinMax) WinMax = temp1;
+                                temp1 = 0;
+                                temp2++;
+                            }
+                        }
+                        if (temp1 > WinMax) WinMax = temp1;
+                        if (temp2 > LoseMax) LoseMax = temp2;
+                        WinsAll += Wins;
+                        LosesAll += Loses;
+                        string LogInBox = (i.ToString() + ".   Всего выигрышей:" + Wins.ToString() + "   Всего проигрышей:" + Loses.ToString());
+                        LogInBox += ("   Наибольшее число побед подряд:" + WinMax.ToString() + "   Наибольшее число проигрышей подряд:" + LoseMax.ToString());
+                        LogInBox += ("   Выигрышей за все время:" + WinsAll.ToString() + "   Проигрышей за все время:" + LosesAll.ToString());
+                        int r = Params[i].HowManyRates - 1;
+                        double sum = Convert.ToDouble((Game[i].Balance[r]).Replace(BadDecSeparator, GoodDecSeparator));
+                        sum -= (Convert.ToDouble((Game[i].Balance[0]).Replace(BadDecSeparator, GoodDecSeparator)));
+                        if (sum >= 0)
+                        {
+                            LogInBox += ("   Вы в плюсе на:" + sum.ToString("F8") + "\n");
+                        }
+                        else LogInBox += ("   Вы в минусе на:" + ((-1) * sum).ToString("F8") + "\n");
+                        richTextBox1.Invoke((MethodInvoker) (() => richTextBox1.Text += LogInBox));
+                        Application.DoEvents();
+                        System.Threading.Thread.Sleep(100);
+                    }
+                    double sum0 = Convert.ToDouble((Game[1].Balance[0]).Replace(BadDecSeparator, GoodDecSeparator));
+                    int posEnd = 1;
+                    for (int k = 1; k < 100; k++)
+                    {
+                        if (Params[k].TypeOfGame == null)
+                        {
+                            posEnd = k - 1;
+                            break;
+                        }
+                    }
+                    double sumEnd = Convert.ToDouble((Game[posEnd].Balance[(Params[posEnd].HowManyRates) - 1]).Replace(BadDecSeparator, GoodDecSeparator));
+                    richTextBox2.Invoke((MethodInvoker) (() => richTextBox2.Text += ("Итог\n")));
+                    richTextBox2.Invoke((MethodInvoker) (() => richTextBox2.Text += ("Сумма на балансе после старта бота: " + sum0.ToString("F8") + "\n")));
+                    richTextBox2.Invoke((MethodInvoker) (() => richTextBox2.Text += ("Сумма на балансе после работы бота: " + sumEnd.ToString("F8") + "\n")));
+                    if (sumEnd>=sum0) richTextBox2.Invoke((MethodInvoker)(() => richTextBox2.Text += ("Вы в плюсе на: " + (sumEnd-sum0).ToString("F8") + "\n")));
+                    else richTextBox2.Invoke((MethodInvoker)(() => richTextBox2.Text += ("Вы в плюсе на: " + (sum0-sumEnd).ToString("F8") + "\n")));
+                    Writer.Close();
+                }
+                Work = new BackgroundWorker();
+                Work.DoWork += (obj, ea) => work();
+                Work.RunWorkerAsync();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка на сайте", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } 
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF1 = new OpenFileDialog();
+            OPF1.Filter = "Файлы .txt|*.txt";
+            if (OPF1.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(OPF1.FileName);
+                string LogsPath = OPF1.FileName;
+                FileTxt1 = new FileStream(LogsPath, FileMode.Open);
+                Writer = new StreamWriter(FileTxt1);
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
